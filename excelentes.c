b4986cbdcec4f6b63905ce6aa7448feb8c21f8fb
Lucas <46837214+foopsss@users.noreply.github.com>
2023-11-18 18:30:30 -0300

    Update excelentes.c

--- excelentes.c
+++ excelentes.c
@@ -12,7 +12,7 @@ struct lugares {
 int eleccion;
 
 void MostrarOpciones() {
-    printf("Elija uno de los destinos: \n");
+    printf("Elija una de las opciones: \n");
     printf("0. Mar del Plata.\n");
     printf("1. Rio de Janeiro.\n");
     printf("2. Buzios.\n");
@@ -24,7 +24,6 @@ void MostrarOpciones() {
     scanf("%d", &eleccion);
     
     // Chequeamos que los valores se encuentren dentro del rango aceptable.
-    // De lo contrario, se considera un error.
     if (eleccion > 5 || eleccion < 0) {
 	clrscr();
 	printf("Usted seleccionó una opción incorrecta.\n");
@@ -53,7 +52,6 @@ void main() {
 	destinos[i].votos = 0;
     }
 
-    // Le ofrecemos a los usuarios la posibilidad de votar.
     MostrarOpciones();
     
     while (eleccion != 5) {
@@ -63,8 +61,7 @@ void main() {
 	MostrarOpciones();
     }
 
-    // Con la votación consumada, reordenamos el arreglo en función de los
-    // lugares más votados, de mayor a menor.
+    // Reordenamos el arreglo en función de los lugares más votados, de mayor a menor.
     while (!bandera) {
 	bandera = true;
 	
@@ -78,7 +75,6 @@ void main() {
 	}
     }
 
-    // Mostramos el ranking luego de ordenar el arreglo con los votos.
     if (cantidadvotos > 0) {
 	clrscr();
 	printf("Ranking de los sitios más votados: \n");
